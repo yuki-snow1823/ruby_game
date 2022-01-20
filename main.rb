3369443e8cbaccf8cd54c6475aa5a7a890565bd3
yuki-snow1823 <y.horikoshi.pg@gmail.com>
2022-01-21 08:23:25 +0900

    [add] セッターの記述

---
+++ main.rb
@@ -0,0 +1,22 @@
+class Brave
+  # nameのセッター
+  def name=(name)
+    @name = name
+  end
+
+  # hpのセッター
+  def hp=(hp)
+    @hp = hp
+  end
+
+  # offenseのセッター
+  def offense=(offense)
+    @offense = offense
+  end
+
+  # defenseのセッター
+  def defense=(defense)
+    @defense = defense
+  end
+
+end
